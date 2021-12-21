fb1405413b4975e95378918dd516459ed9cfacb6
Guna <guna.tw@gmail.com>
2021-12-21 17:33:04 +0530

    code formatting done

--- src/main/java/com/javatpoint/repository/BooksRepository.java
+++ src/main/java/com/javatpoint/repository/BooksRepository.java
@@ -1,7 +1,10 @@
 package com.javatpoint.repository;
+
 import org.springframework.data.repository.CrudRepository;
-//repository that extends CrudRepository
 import com.javatpoint.model.Books;
-public interface BooksRepository extends CrudRepository<Books, Integer>
-{
+
+//repository that extends CrudRepository
+
+
+public interface BooksRepository extends CrudRepository<Books, Integer> {
 }
